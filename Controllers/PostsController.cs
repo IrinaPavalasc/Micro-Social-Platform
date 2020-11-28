19c8ab71e6af329f444f26799e06029e42f8f976
IrinaPavalasc <irinapavalasc@yahoo.ro>
2020-11-28 15:09:22 +0200

    testIrina

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -8,7 +8,8 @@ using System.Web.Mvc;
 namespace Micro_Social_Platform.Controllers
 {
     public class PostsController : Controller
-    {   //testGitHub
+    {   //testGitHub2
+        //testGitHubIrina
         private Micro_Social_Platform.Models.ApplicationDbContext db = new Micro_Social_Platform.Models.ApplicationDbContext();
         // GET: Posts
         public ActionResult Index()
